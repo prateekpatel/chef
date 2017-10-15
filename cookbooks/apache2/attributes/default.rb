cee271f624614b05ebdf7c8b71af9e66dc96fd9d
root <root@shaurya23094.mylabserver.com>
2017-10-15 11:50:26 +0000

    this is new apache cookbook with virtual hosting

---
+++ cookbooks/apache2/attributes/default.rb
@@ -0,0 +1,2 @@
+default["apache"]["sites"]["shaurya23095"] = { "port" => 80, "domain" => "shaurya23095.mylabserver.com" }
+default["apache"]["sites"]["shaurya23096"] = { "port" => 80, "domain" => "shaurya23096.mylabserver.com" }
